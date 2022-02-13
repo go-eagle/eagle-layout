3019a225cda7383548c302c19337ee344b435c21
qloog <quanlongwang@gmail.com>
2022-02-13 22:34:24 +0800

    chore: add default depend resource

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,15 +1,9 @@
 package repository
 
 import (
-	"github.com/google/wire"
-	"gorm.io/gorm"
-
 	"github.com/go-eagle/eagle-layout/internal/model"
+	"github.com/google/wire"
 )
 
 // ProviderSet is repo providers.
-var ProviderSet = wire.NewSet(NewGORMClient)
-
-func NewGORMClient() *gorm.DB {
-	return model.GetDB()
-}
+var ProviderSet = wire.NewSet(model.Init())
